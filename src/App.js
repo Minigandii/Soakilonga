f4698a4b013798efa08c189af72940cce161ab0b
Minigandii <clement.gandillon@gmail.com>
2025-02-04 21:12:54 +0100

    add scroll

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Accueil from './pages/Accueil';
-import NousDecouvrir from './pages/NousDecouvrir';
-import NosActions from './pages/NosActions';
-import Actualite from './pages/Actualite';
-import NousSoutenir from './pages/NousSoutenir';
-import Contact from './pages/Contact';
-import Centres from './pages/Centres';
-import Navbar from './components/Navbar';
-import './tailwind.css';
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Accueil from "./pages/Accueil";
+import NousDecouvrir from "./pages/NousDecouvrir";
+import NosActions from "./pages/NosActions";
+import Actualite from "./pages/Actualite";
+import NousSoutenir from "./pages/NousSoutenir";
+import Contact from "./pages/Contact";
+import Centres from "./pages/Centres";
+import Navbar from "./components/Navbar";
+import "./tailwind.css";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <ScrollToTop />
+      <Navbar />
       <div className="App">
         <Routes>
           <Route path="/" element={<Accueil />} />
